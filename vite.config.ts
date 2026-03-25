6b856755f4c47640b9d87d726b3368dafaa638ee
Digital Expert <mr.gonzalex.4@gmail.com>
2026-03-25 16:15:13 -0600

    fix: final path correction with leading slash

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portafolio/', // Asegúrate de que tenga las dos barras "/"
+  base: '/portafolio/', // Con las dos barras
 })
